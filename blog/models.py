f60adeca42ccd3eef35318899973d4860bdad26a
lee <lihaosyd@gmail.com>
2017-07-25 00:26:13 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    #文章标题
+    title = models.CharField(max_length=70)
+    #文章正文
+    body = models.TextField()
+    #文章创建时间和最后修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    #文章摘要
+    excerpt = models.CharField(max_length=200,blank=True)
+    #
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag,blank=True)
+    #文章作者
+    author = models.ForeignKey(User)
+    def __str__(self):
+        return self.title
